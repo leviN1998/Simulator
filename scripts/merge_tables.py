592c6819719869cf54e486c4e5cc9c3e123b07a1
Levin Kolmar <levin.kolmar@googlemail.com>
2025-10-10 14:40:50 +0200

    updates

--- scripts/merge_tables.py
+++ scripts/merge_tables.py
@@ -4,7 +4,7 @@ import numpy as np
 
 path = "/data/lkolmar/datasets/realistic_topspin/"
 
-num_threads = 9
+num_threads = 10
 index_offset = 0 # does the first thread start at 0 or 1?
 
 if __name__ == "__main__":
